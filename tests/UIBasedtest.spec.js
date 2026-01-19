0a09f49c0d8c58fc55516527ca77dcfbf9ae49ee
Gowri Prabhu <gowriprabhu.sp@gmail.com>
2026-01-19 19:45:58 +0530

    Dropdown, radiobutton selection and assertions

--- tests/UIBasedtest.spec.js
+++ tests/UIBasedtest.spec.js
@@ -1,6 +1,6 @@
 const {test, expect} =require('@playwright/test');
 
-test.only('BrowserContext playwright test', async ({browser}) => {
+test('BrowserContext playwright test', async ({browser}) => {
 
 const context = await browser.newContext();
 const page = await context.newPage();
@@ -27,9 +27,33 @@ console.log(ItemTitles);
 
 });
 
-test('General playwright test', async ({page}) => {
-await page.goto("https://rahulshettyacademy.com/");
-console.log("The extracted title is: "+await page.title());
-await expect(page).toHaveTitle("Rahul Shetty Academy | QA Automation, Playwright, AI Testing & QA Online Training");
-//expect(await page.title()).toBe("rahulshettyacademy.com");
+test.only('UI controls', async ({page}) => {
+// await page.goto("https://rahulshettyacademy.com/");
+// console.log("The extracted title is: "+await page.title());
+// await expect(page).toHaveTitle("Rahul Shetty Academy | QA Automation, Playwright, AI Testing & QA Online Training");
+// //expect(await page.title()).toBe("rahulshettyacademy.com");
+const username =  page.locator("#username");
+const password =  page.locator("#password");
+const usertypeRadioBtn =  page.locator("span.checkmark");
+const dropdown =  page.locator("select.form-control");
+const material = page.locator("a.blinkingText").first();
+const signInBtn =  page.locator("#signInBtn");
+const productdetailsCards =  page.locator(".card-body a");
+await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+console.log(await page.title());
+await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
+await usertypeRadioBtn.nth(1).click();
+await dropdown.selectOption("consult");
+await page.locator("#okayBtn").click();
+await expect(usertypeRadioBtn.nth(1)).toBeChecked();
+console.log(await usertypeRadioBtn.nth(1).isChecked());
+await page.locator("#terms").click();
+await expect(page.locator("#terms")).toBeChecked();
+await page.locator("#terms").uncheck();
+await expect(page.locator("#terms")).not.toBeChecked();
+//expect(await page.locator("#terms").isChecked()).toBeFalsy();
+await expect(material).toHaveAttribute("class", "blinkingText");
+
+
+
 });
